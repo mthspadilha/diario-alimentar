acf93e21fc26f92df6b246048ad458af4b344821
mthspadilha <matheuspadilha.m@gmail.com>
2022-03-23 23:41:10 -0300

    mappings

--- src/main/java/dev/padda/diarioalimentar/service/NutricionistaService.java
+++ src/main/java/dev/padda/diarioalimentar/service/NutricionistaService.java
@@ -2,12 +2,15 @@ package dev.padda.diarioalimentar.service;
 
 import dev.padda.diarioalimentar.model.Nutricionista;
 import dev.padda.diarioalimentar.repository.NutricionistaRepo;
-import org.graalvm.compiler.core.common.NumUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import dev.padda.diarioalimentar.exception.RegisterNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/nutri")
 public class NutricionistaService {
     private final NutricionistaRepo nutricionistaRepo;
 
@@ -16,18 +19,22 @@ public class NutricionistaService {
         this.nutricionistaRepo = nutricionistaRepo;
     }
 
+    @PostMapping("/adicionar")
     public Nutricionista saveNutricionista(Nutricionista nutricionista){
         return nutricionistaRepo.save(nutricionista);
     }
 
+    @GetMapping("/lista")
     public List<Nutricionista> findAllNutricionista(){
         return nutricionistaRepo.findAll();
     }
 
+    @GetMapping("/edita")
     public Nutricionista updateNutricionista(Nutricionista nutricionista){
         return nutricionistaRepo.save(nutricionista);
     }
 
+    @GetMapping("/lista/{id}")
     public Nutricionista findNutricionistaById(Long id){
         return nutricionistaRepo.findNutricionistaById(id).orElseThrow(()-> new RegisterNotFoundException("Nutri não encontrada"));
     }
